eb39a344c423a0a7daa606413ead5c882b67b0e9
Daniel Bahrdt <daniel.git@funroll-loops.de>
2018-07-25 15:15:59 +0200

    [website] don't show clusters above given zoom level

--- website/js/map/map.js
+++ website/js/map/map.js
@@ -1912,7 +1912,7 @@ function (require, state, $, config, oscar, flickr, tools, tree) {
 					
 					var childNode = state.dag.region(childId);
 					var myOverlap = tools.percentOfOverlap(state.map, childNode.bbox);
-					if (myOverlap >= 0 && state.map.getZoom() === state.map.getMaxZoom()) {
+					if (myOverlap >= 0 && state.map.getZoom() >== map.cfg.clustering.maxZoomLevel) {
 						myOverlap = 100;
 					}
 
